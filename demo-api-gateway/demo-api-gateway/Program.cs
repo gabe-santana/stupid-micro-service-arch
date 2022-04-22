ca30c9ed93e5a9445f83c371889a9a295dcf8594
Gabriel Santana <gsantana@bdsdatasolution.com.br>
2022-04-22 11:09:26 -0300

    Authentication added

--- demo-api-gateway/demo-api-gateway/Program.cs
+++ demo-api-gateway/demo-api-gateway/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +21,44 @@ IConfiguration configuration = new ConfigurationBuilder()
                             .Build();
 
 builder.Services.AddOcelot(configuration);
+
+builder.Services.AddAuthentication(options =>
+{   
+    // Store the session to cookies
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    // OpenId authentication
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "http://localhost:8080/auth/realms/Production/protocol/openid-connect/auth";
+        options.Audience = "api1";
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            NameClaimType = "name",
+            RoleClaimType = "role"
+        };
+    })
+    .AddCookie("Cookies")
+    .AddOpenIdConnect(options =>
+    {
+        // URL of the Keycloak server
+        options.Authority = "http://localhost:8080/auth/realms/Production/protocol/openid-connect/auth";
+        // Client configured in the Keycloak
+        options.ClientId = "myClient";
+
+        // For testing we disable https (should be true for production)
+        options.RequireHttpsMetadata = false;
+        options.SaveTokens = true;
+        options.ClientId = "myClient";
+        // Client secret shared with Keycloak
+        options.ClientSecret = "8efae46d-e1f2-43bc-a810-d0ef6d961008";
+        options.GetClaimsFromUserInfoEndpoint = true;
+
+        // OpenID flow to use
+        options.ResponseType = OpenIdConnectResponseType.CodeIdToken;
+    });
 
 var app = builder.Build();
 
@@ -31,5 +76,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
